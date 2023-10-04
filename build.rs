19263fb5c716d002e8735292f0ef1acdaeeb0a13
m8 <umusasadik@gmail.com>
2023-10-04 20:05:16 +0200

    Add libatomic for gcc

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ fn main() {
     let dst = Config::new("libverona").build();
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=static=verona");
-    //println!("cargo:rustc-link-lib=static=stdc++");
-    println!("cargo:rustc-link-lib=static=c++");
+    println!("cargo:rustc-link-lib=static=stdc++");
+    // println!("cargo:rustc-link-lib=static=c++");
+    println!("cargo:rustc-link-lib=atomic");
 }
